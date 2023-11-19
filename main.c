ca708f31fcbafcb0e8575e1a86d0871118129168
sjhe <sjhe@kns.com>
2023-11-19 21:58:12 +0800

    add *.sh files

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include "sj_lib/foo.h"  /* Include the header here, to obtain the function declaration */
+#include "lib/foo.h"  /* Include the header here, to obtain the function declaration */
 
 int main(void)
 {
     int y = foo(3);  /* Use the function here */
     printf("Hello World!\n");
     printf("%d\n", y);
+    printf("Hello World!\n");
     return 0;
 }
