8396ee12383a89839e7762aeb231ce90d0725e75
sjhe <sjhe@kns.com>
2023-11-19 17:21:20 +0800

    to test library

---
+++ sj_src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../sj_lib/foo.h"  /* Include the header here, to obtain the function declaration */
+
+int main(void)
+{
+    int y = foo(3);  /* Use the function here */
+    printf("Hello World!\n");
+    printf("%d\n", y);
+    return 0;
+}
